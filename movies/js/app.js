885b8b0c4419efaacb6cc8fe646195c9ffdbdb57
Dave Stearns <dlsinfo@uw.edu>
2016-03-25 15:10:19 -0700

    starter files for movies challenge

---
+++ movies/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * app.js
+ * main application script for the movies challenge
+ * add code here to calculate the various metrics
+ * required by the challenge, and add them to the
+ * tables already created in index.html
+ * the array of movies is available via the global
+ * variable `movies` (see data/movies-2015.js)
+ */
